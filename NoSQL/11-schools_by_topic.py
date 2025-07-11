9bfb14b4b7115d8701af06f0a53c7eb2fa29fa4e
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-07-11 13:11:19 +0000

    11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' list of schools with spesific topic '''
+
+
+def schools_by_topic(mongo_collection, topic):
+    ''' function '''
+    return list(mongo_collection.find({"topic": topic}))
